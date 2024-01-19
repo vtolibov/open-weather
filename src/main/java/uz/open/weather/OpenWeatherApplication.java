fefb85393a2e07777439d6f69cc96c1c2bc2139d
vakhobjon.tolibov <vakhobjon.tolibov@paynet.uz>
2024-01-19 17:22:24 +0500

    Initial commit

---
+++ src/main/java/uz/open/weather/OpenWeatherApplication.java
@@ -0,0 +1,13 @@
+package uz.open.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/open/weather/OpenWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.open.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

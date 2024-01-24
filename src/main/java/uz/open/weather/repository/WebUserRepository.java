4d8511fa3cefce5ed1ac108483752ed74123cc46
vakhobjon.tolibov <vakhobjon.tolibov@paynet.uz>
2024-01-24 17:26:34 +0500

    added some methods to UserManagementService, Weather and Subscriptions services

--- src/main/java/uz/open/weather/repository/WebUserRepository.java
+++ src/main/java/uz/open/weather/repository/WebUserRepository.java
@@ -1,13 +1,10 @@
 package uz.open.weather.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import uz.open.weather.dto.auth.AuthUser;
 import uz.open.weather.dto.user.WebUser;
 
 import java.util.Optional;
 
-@Repository
 public interface WebUserRepository extends JpaRepository<WebUser, Long> {
     Optional<WebUser> findByUsername(String username);
 }
